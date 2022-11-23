32aaec0416b20a9515966402e9e39a75db3d026b
Emilia Peñaloza <58234390+NokutokaMomiji@users.noreply.github.com>
2022-11-23 13:40:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# C_Template
+ A very simple, basic templace for C programs with a Makefile included.
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+#include "common.h"
+
+int main(int argc, const char* argv[]) {
+    printf(".\n");
+    return 0;
+}
